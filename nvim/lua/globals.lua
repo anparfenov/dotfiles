041e0ec8873cdf6e6df704dd946501784e05aabf
Andrey Parfenov <asleeppiano@outlook.com>
2022-04-13 22:03:13 +0300

    adds nvim lua files

---
+++ nvim/lua/globals.lua
@@ -0,0 +1,17 @@
+MY = { functions = {} }
+
+function MY.functions.change_theme()
+    local dark_theme = 'aurora'
+    local light_theme = 'inspired-github'
+    local default_theme = 'everforest'
+    local current_theme = vim.g.colors_name
+    if current_theme == dark_theme then
+        current_theme = light_theme
+    elseif current_theme == light_theme then
+        current_theme = dark_theme
+    else
+        current_theme = default_theme
+    end
+    local cmd = 'colorscheme ' .. current_theme
+    vim.cmd(cmd)
+end
